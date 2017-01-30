c6cfd1178780a65531ca91001764cac60db5889e
Daniel Chan <dchan@intouchsol.com>
2017-01-30 11:17:59 -0600

    new script and forgot the ; in the test one

---
+++ move_annotations.js
@@ -0,0 +1,10 @@
+/* this is for "pulling over" annotations from one file to another with major similarities 
+user inputs must be manually entered into the var fields
+
+USER INPUTS:
+
+*/
+
+var tpages = 
+
+/* END USER INPUT SECTION*/
