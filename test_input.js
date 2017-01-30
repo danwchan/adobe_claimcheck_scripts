33a568e92895f7e75b02e041e857f5489fc4d309
Daniel Chan <dchan@intouchsol.com>
2017-01-30 11:30:41 -0600

    making the console command work

--- test_input.js
+++ test_input.js
@@ -1,7 +1,9 @@
 /*This is a test script to see if Adobe will allow user inputs as pop-ups*/
 
-var teststatus = window.confirm("testing this");
+var testvar = "this is my test var";
 var testinput = window.prompt("make a variable!","default var");
-console.printIn(teststatus + testinput);
+console.clear();
+console.show();
+console.printIn(testinput + testvar);
 
 /*The answer is NO, the settings of the script must be set out in the inital code which can be made editable by checking the "Prompt User" button*/
