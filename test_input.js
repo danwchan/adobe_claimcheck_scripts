5e9659c2a54a691928f6f15f3c74f735857ff6e1
Daniel Chan <dchan@intouchsol.com>
2017-01-30 11:09:28 -0600

    updated the test files and made a script to test user input, adobe seems to not allow popups

---
+++ test_input.js
@@ -0,0 +1,8 @@
+/*This is a test script to see if Adobe will allow user inputs as pop-ups*/
+
+var teststatus = window.confirm("testing this")
+var testinput = window.prompt("make a variable!","default var")
+window.alert(teststatus)
+window.alert(testinput)
+
+/*The answer is NO, the settings of the script must be set out in the inital code which can be made editable by checking the "Prompt User" button*/
