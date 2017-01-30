4ab43c7bd27750a09c22c560d6a4a12ad56f3762
Daniel Chan <dchan@intouchsol.com>
2017-01-30 12:24:21 -0600

    dialog box controls

--- test_input.js
+++ test_input.js
@@ -5,4 +5,51 @@ console.clear();
 console.show();
 console.println(testvar);
 
+var dialog1 = {
+
+    initialize: function (dialog) {
+        var input = dialog.store()["inputtext"];
+        input = "the input";
+        dialog.load({ "inputtext": input});
+    },
+    commit: function (dialog) {
+        var results = dialog.store();
+        console.println("The inputed text was: " + results["input"])
+    },
+    description:
+     {
+        name: "The INPUT Box",
+        align_children: "align_left",
+        width: 350,
+        height: 200,
+        elements:
+        [
+             {
+             type: "cluster",
+             name: "The place you put input"
+             align_children: "align left",
+             elements:
+             [
+                 {
+                 type: "static_text",
+                 name: "Input: "
+                 },
+                 {
+                 item_id: "input",
+                 type: "edit_text",
+                 alignment: "align_fill",
+                 width: 300,
+                 height: 20
+                 }
+             ]
+             },
+             {
+             type: "static_text"
+             name: "some other text you wanted in this cluster"
+             },
+         ]
+     };
+
+app.execDialog(dialog1);
+
 /*The answer is NO, the settings of the script must be set out in the inital code which can be made editable by checking the "Prompt User" button*/
