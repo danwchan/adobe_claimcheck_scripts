151d77465c866cdef8a57c60ee89992e10ed75ad
Daniel Chan <dchan@intouchsol.com>
2017-01-30 11:38:38 -0600

    this is how you print out messages

--- test_input.js
+++ test_input.js
@@ -1,9 +1,8 @@
 /*This is a test script to see if Adobe will allow user inputs as pop-ups*/
 
 var testvar = "this is my test var";
-var testinput = window.prompt("make a variable!","default var");
 console.clear();
 console.show();
-console.printIn(testinput + testvar);
+console.println(testvar);
 
 /*The answer is NO, the settings of the script must be set out in the inital code which can be made editable by checking the "Prompt User" button*/
