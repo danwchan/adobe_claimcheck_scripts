61ab96c3ce27d8c1d9f2eacf5300e529686a9e4d
Daniel Chan <dchan@intouchsol.com>
2017-01-31 13:24:47 -0600

    a working toy example of a dialog box that takes input and performs a function

--- test_input.js
+++ test_input.js
@@ -2,43 +2,63 @@
 
 var testvar = "this is my test var";
 
+//a function to be called on the inputs from inside the dialog
+function catter(numberOne, numberTwo) {
+    return (numberOne) + (numberTwo);
+}
+
 // Dialog Definition 
 var oDlg = {
 
+//set the variables
+    input0: "",
     input1: "",
-
+    
+//initialize the dialog by loading the variables
     initialize: function(dialog) {
         var listofinput = {
-             "inpt":this.input1
+             "var0":this.input0,
+             "var1":this.input1
         }
         dialog.load(listofinput);
     },
 
+/*commit isnt required because I have the ok: defined
+//specify the way the varibles are assigned values, store the input
     commit: function(dialog) {
         var data = dialog.store(); 
-        this.strName = data[ "usnm"]; 
+        this.input0 = data[ "var0"];
+        this.input1 = data[ "var1"];
+    },
+*/
+
+//the OK button action
+    ok: function(dialog) {
+        var data = dialog.store(); 
+        this.input0 = data[ "var0"];
+        this.input1 = data[ "var1"];
+        console.println("Input recorded");
+        console.println(catter(data["var0"], data["var1"]));
     },
 
+//how the box should look and which areas corresspond to which inputs
     description: {
         name: "Test Dialog", elements: [ {
             type: "view", elements: [
-                { name: "Enter your name:", type: "static_text", },
-                { item_id: "usnm", type: "edit_text", char_width: 15 },
-                { type: "ok_cancel", },
+                { name: "Input:", type: "static_text", },
+                { item_id: "var0", type: "edit_text", char_width: 15 },
+                { name: "Input2:", type: "static_text", },
+                { item_id: "var1", type: "edit_text", char_width: 15 },
+                { type: "ok", ok_name: "submit"},
             ]
         },]
     }
 };	
 
 // Dialog Activation 
-app.execDialog(oDlg);
-
-
 console.clear();
 console.show();
-
-console.println(testvar);
-
-console.print1n(data["inpt"]);
+app.execDialog(oDlg);
+console.println(data);
 
 /*The answer is NO, the settings of the script must be set out in the inital code which can be made editable by checking the "Prompt User" button*/
