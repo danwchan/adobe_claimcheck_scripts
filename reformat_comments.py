030601e745aaab5b4d3fb9b07e631588f90fec7d
Daniel Chan <dchan@intouchsol.com>
2017-02-03 11:33:23 -0600

    start the script to sift though comments, starting with the RE required

---
+++ reformat_comments.py
@@ -0,0 +1,10 @@
+import re
+
+test = open("C:\\Users\\dchan\\Documents\\adobe_claimcheck_scripts\\testcomments_diversityfile.txt", "r")
+
+counter = 0
+
+for line in test:
+    counter = counter + 1
+
+print (counter)
