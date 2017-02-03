eed017e24985b42ff5448a74e0df3c954f35e5ab
Daniel Chan <dchan@intouchsol.com>
2017-02-03 14:11:58 -0600

    got the authors to print out, decided to make the script good enough i.e. exceptions will just be printed out

--- reformat_comments.py
+++ reformat_comments.py
@@ -1,10 +1,33 @@
 import re
 
-test = open("C:\\Users\\dchan\\Documents\\adobe_claimcheck_scripts\\testcomments_diversityfile.txt", "r")
+
+def splitter(file):
+    comments = file.readlines()
+
+def builder(lookup_list):
+    
+    
 
 counter = 0
 
-for line in test:
-    counter = counter + 1
+lookup_author = re.compile(r"^([\w\s]*?(?=\d{4} )).*") #looking for the author only if it is followed by the year
+
+authors = [lookup_author.match(lines.strip()) for lines in comments]
+
+#a little test for the properties of .match
+if authors[2]== False:
+	print("it's false")
+else:
+	print("there's something here")
+
+
+for lines in comments:
+    authors = lookup_author.match(lines.strip())
+
+with open("C:\\Users\\dchan\\Documents\\adobe_claimcheck_scripts\\testcomments_diversityfile.txt", "r") as f:
+    for line in f:
+         counter = counter + 1# read into a list and operate over each
 
 print (counter)
+
+
