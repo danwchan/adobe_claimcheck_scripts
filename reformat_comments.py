e05c3746502f8f47de338621ef3463175a0f7b72
Daniel Chan <dchan@intouchsol.com>
2017-02-13 17:29:12 -0600

    using readall to iterate over the string

--- reformat_comments.py
+++ reformat_comments.py
@@ -16,11 +16,14 @@ Regular expression help comments:
 ^([\w\s]*?(?=(\d{4})[ ,])) <- looking for the author and year only if it is followed by the year
 ([pP][aA]?[gG]?[eE]?[ ]?)(\w{1,5})[, ].*?\1(\w{1,5})[, ].*?<- looking for the page
 '''
-lookup_authoryear = re.compile(r"^([\w\s]*?(?=(\d{4})[ ,])).*") 
-lookup_page = re.compile(r"(((?:.+?[pP][aA]?[gG]?[eE]?[ ]?)([vVsSi\d]{1,6})[, ])+).*?") #hitting th problem that I can't capure the repeated page number
+lookup_authoryear = re.compile(r"^([\w\s]*?(?=(\d{4})[ ,]))") 
+lookup_page = re.compile(r"(?:.*?[pP][aA]?[gG]?[eE]?[ ]?)([vVsSi\d]{1,6})[, ]") #hitting th problem that I can't capure the repeated page number
 
-authors = [lookup_author.match(lines.strip()) for lines in annotations]
-pages = [lookup_page.match(lines.strip()) for lines in annotations]
+authors = [lookup_author.findall(lines.strip()) for lines in annotations]
+
+annotations = [lookup_author.sub("author", lines.strip()) for lines in annotations] #remove the author to ease regex confusion
+
+pages = [lookup_page.findall(lines.strip()) for lines in annotations]
 
 #a little test for the properties of .match
 if authors[2]== None:
@@ -33,6 +36,15 @@ counter = 0
 for lines in pages:
     if lines != None:
         lines.groups()
+        counter = counter + 1
+    else:
+        print ("search empty :( for index %d" % (counter))
+        counter = counter + 1
+
+#another littles test for the properties of .match
+for lines in pages:
+    if lines != None:
+        print(lines)
         counter = counter + 1
     else:
         print ("search empty :( for index %d" % (counter))
